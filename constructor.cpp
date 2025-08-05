f4f8dc2c72aea00f56401d524071e5c56c1e8562
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-06 00:07:28 +0530

    Point Constructor

--- constructor.cpp
+++ constructor.cpp
@@ -94,3 +94,31 @@ int main()
 
 
 */
+
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+public:
+    Point(int a, int b){
+        x = a;
+        y = b;
+    }
+    void displayPoint() { 
+        cout << "The point is (" << x << "," << y << ")" << endl; 
+    }
+};
+int main(){
+    Point p(1, 4);
+    p.displayPoint();
+
+    Point q(5, 9);
+    q.displayPoint();
+    return 0;
+}
+
+
+
+
+// Constructor Overloading 
