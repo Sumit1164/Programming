6c277fcf4ce93bc6c01a649d17e35b3403b52e02
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-10 17:55:05 +0530

    Constructor with default arguments program

--- constructor.cpp
+++ constructor.cpp
@@ -153,3 +153,23 @@ int main(){
 // Constructor with default arguments
 
 */
+#include<iostream>
+using namespace std;
+class Simple{
+    int data1;
+    int data2;
+    public:
+        Simple(int a, int b = 9){
+            data1 = a;
+            data2 = b;
+        }
+    void printData(){
+        cout << "The value of "<< data1 <<" and "<< data2 << endl;
+        cout << "The sum of A + B = " << data1 + data2 << endl;
+    }
+};
+int main(){
+    Simple s(3);
+    s.printData();
+    return 0;
+}
