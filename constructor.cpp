82d8c883e5654f157a6b4d20f8060995d2c4872d
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-10 22:29:50 +0530

    Dynamic Initialization of Object Using Constructor

--- constructor.cpp
+++ constructor.cpp
@@ -180,4 +180,71 @@ int main(){
 
 // Dynamic Initialization of Object Using Constructor
 
+
+
+#include<iostream>
+using namespace std;
+
+class BankDeposit{
+    int principle;
+    int years;
+    float intrestRate;
+    float returnValue;
+public:
+    BankDeposit(){}
+    BankDeposit(int p, int y, float r);
+    BankDeposit(int p, int y, int r);
+    void show();
+};
+
+BankDeposit :: BankDeposit(int p, int y, float r){
+    principle = p;
+    years = y;
+    intrestRate = r;
+    returnValue = principle;
+    for (int i = 0; i < y; i++){
+        returnValue = returnValue * (1 + intrestRate);
+    }
+}
+
+BankDeposit :: BankDeposit(int p, int y, int r)
+{
+    principle = p;
+    years = y;
+    intrestRate = float(r) / 100;
+    returnValue = principle;
+    for (int i = 0; i < y; i++)
+    {
+        returnValue = returnValue * (1 + intrestRate);
+    }
+}
+
+void BankDeposit :: show(){
+    cout << "Principle amount was " << principle
+    <<". Return value after "<<years
+    <<" years is: "<<returnValue<<endl;
+}
+
+
+int main(){
+    BankDeposit bd1, bd2, bd3;
+    int p, y;
+    float r;
+    int R;
+
+    bd3.show();
+    cout << "Enter the value of p, y, r: ";
+    cin >> p >> y >> r;
+    bd1 = BankDeposit(p, y, r);
+    bd1.show();
+    cout << "Enter the value of p, y, R: ";
+    cin >> p >> y >> R;
+    bd2 = BankDeposit(p, y, R);
+    bd2.show();
+    return 0;
+}
+
+
+// Copy Constructor
+
 */
