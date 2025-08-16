61f62709cb980893fe14778bd65920a769a071a6
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-16 22:30:47 +0530

    Inheritance  exapmle foe single inheritance

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -21,21 +21,23 @@
 */
 
 
-
+/*
 #include<iostream>
 using namespace std;
 
 // Base Class
 class Emp{
-    int id;
-    
     public:
+        int id;
         float salary;
-        Emp(int intId){
-            id = intId;
+        Emp(int inpId){
+            id = inpId;
             salary = 8888.9;
         }
+        Emp(){}
 };
+
+*/
 
 /*
 //    ----------------------------->  Derived Class Syntax   <--------------------------------------------
@@ -43,20 +45,161 @@ class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}{
     class members/methods/etc...
 }
 
-----------> Note : Default visibility-mode is private.
+-----> Note : Default visibility-mode is private.
 -----> Pubic visibility Mode : Public member of the base class become Public member of the derived class
 -----> Private visibility Mode : Public member of the base class become Private member of the derived class
 -----> Private members are never inherited derived class 
 */
+
+/*
 class Engineer : Emp{
     public:
+    Engineer(int inpId){
+        id = inpId;
+    }
         int role = 57;
-}
+};
 
 int main(){
     Emp Sumit(1), Kasu(2);
     cout << Sumit.salary << endl;
     cout << Kasu.salary << endl;
     Engineer skillK(1);
+    cout<<skillK.role;
+    return 0;
+}
+
+
+
+// Example of Single Inheritance
+#include<iostream>
+using namespace std;
+
+class BaseC{
+public:
+    int a, b;
+    void sum(){
+        cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+    }
+};
+class DerivedC : public BaseC{
+public:
+    void printsum(){
+        cout << "Sum of two number, using single inheritance" << endl;
+    }
+};
+
+int main(){
+    DerivedC obj;
+    obj.a = 57;
+    obj.b = 59;
+
+    obj.sum();
+    obj.printsum();
+    return 0;
+}
+
+
+
+// For Public Inheritance
+
+#include<iostream>
+using namespace std;
+class Base{
+    int data1;
+public:
+    int data2;
+    void setData();
+    int getData1();
+    int getData2();
+};
+void Base :: setData(void){
+    data1 = 5;
+    data2 = 7;
+}
+int Base :: getData1(){
+    return data1;
+}
+int Base ::getData2(){
+    return data2;
+}
+class Derived : public Base{
+    int data3;
+public:
+    void process();
+    void display();
+};
+void Derived :: process(){
+    data3 = data2 * getData1();
+}
+void Derived ::display(){
+    cout << "Value of data1 is " << getData1() << endl;
+    cout << "Value of data1 is " << data2 << endl;
+    cout << "Value of data1 is " << data3 << endl;
+}
+
+int main(){
+    Derived der;
+    der.setData();
+    der.process();
+    der.display();
+    return 0;
+}
+
+
+*/
+
+// For Private Inheritance
+
+#include <iostream>
+using namespace std;
+class Base
+{
+    int data1;
+
+public:
+    int data2;
+    void setData();
+    int getData1();
+    int getData2();
+};
+void Base ::setData(void)
+{
+    data1 = 5;
+    data2 = 7;
+}
+int Base ::getData1()
+{
+    return data1;
+}
+int Base ::getData2()
+{
+    return data2;
+}
+class Derived : private Base
+{
+    int data3;
+
+public:
+    void process();
+    void display();
+};
+void Derived ::process()
+{
+    setData();
+    data3 = data2 * getData1();
+}
+void Derived ::display()
+{
+    cout << "Value of data1 is " << getData1() << endl;
+    cout << "Value of data1 is " << data2 << endl;
+    cout << "Value of data1 is " << data3 << endl;
+}
+
+int main()
+{
+    Derived der; 
+    der.process();
+    der.display();
     return 0;
 }
