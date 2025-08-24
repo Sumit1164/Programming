a615896a2feb4a2036ac155b073ea12e4cf9686f
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-25 00:51:00 +0530

    For Protected Inheritance and defination as well as example

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -147,7 +147,7 @@ int main(){
 }
 
 
-*/
+
 
 // For Private Inheritance
 
@@ -198,8 +198,36 @@ void Derived ::display()
 
 int main()
 {
-    Derived der; 
+    Derived der;
     der.process();
     der.display();
+    return 0;
+}
+
+*/
+
+// For Protected Inheritance
+#include<iostream>
+using namespace std;
+class Base{
+protected:
+    int a;
+private:
+    int b;
+};
+/*
+For a protected member:
+                            Public derivation      Private derivation     Protected derivation
+    1. Private members         Not Inherited          Not Inherited          Not Inherited
+    2. Protected members       Protected              Private                Protected 
+    3. Public members          Public                 Private                Protected
+*/
+class Derived : protected Base{
+
+};
+int main() {
+    Base b;
+    Derived d;
+    // cout << d.a;           -> this show will error because a is protected member 
     return 0;
 }
