0729ca9e7e2e58820bfdeedfffd8375d67940051
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-27 11:42:39 +0530

    nother Example of multilevel inheritance

--- MultiLevelInheritance.cpp
+++ MultiLevelInheritance.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include<iostream>
 using namespace std;
 
@@ -31,5 +33,52 @@ int main(){
     obj.Father ::display();
     obj.Father ::GrandFather ::dy();
     cout << "Code Competions: " << endl;
+    return 0;
+}
+
+-----------------------> Another Example of multilevel inheritance <-------------------------------
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "This animal eats food." << endl;
+    }
+};
+
+// Derived class from Animal 
+class Dog : public Animal
+{
+public:
+    void bark()
+    {
+        cout << "The dog barks." << endl;
+    }
+};
+
+// Derived class from Dog
+class Puppy : public Dog
+{
+public:
+    void weep()
+    {
+        cout << "The puppy weeps." << endl;
+    }
+};
+
+int main()
+{
+    Puppy p;
+    p.eat();  // inherited from Animal
+    p.bark(); // inherited from Dog
+    p.weep(); // own method
+
     return 0;
 }
