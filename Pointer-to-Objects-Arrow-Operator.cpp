6872472582a688f81bb8253c423a7c7d26fb9d3f
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-22 23:48:05 +0530

    Add implementation of Complex class with pointer usage and data handling

---
+++ Pointer-to-Objects-Arrow-Operator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Complex{
+    int real, imaginary;
+    public:
+        void getData(){
+            cout << "The real part is " << real << endl;
+            cout << "The imaginary part is " << imaginary << endl;
+        }
+        void setData(int a, int b){
+            real = a;
+            imaginary = b;
+        }
+};
+int main(){
+    // Complex c1;
+    // Complex *ptr = &c1;
+    Complex *ptr = new Complex;
+    // (*ptr).setData(5, 7);
+    // Arrow operaor (dereferencing)
+    ptr->setData(7, 5);
+    (*ptr).getData();
+
+
+
+    // array of objects 
+    return 0;
+}
