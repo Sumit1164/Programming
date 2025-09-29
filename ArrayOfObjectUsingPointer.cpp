f553dd481e59ad99a191053b9ad12e8071ed2d91
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-30 00:52:17 +0530

    Implement code changes to enhance functionality and improve performance

--- ArrayOfObjectUsingPointer.cpp
+++ ArrayOfObjectUsingPointer.cpp
@@ -15,7 +15,25 @@ class Shop{
 };
 int main(){
     int size = 3;
-    int *ptr = &size;
+    // int *ptr = &size;
     Shop *ptr = new Shop[size];
+    Shop *ptrTemp = ptr;
+    int s;
+    float k;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Id and price of item " << i + 1 << endl;
+        cin >> s >> k;
+        // (*ptr).setData(s, k);
+        ptr->setData(s, k);
+        ptr++;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Item number: " << i + 1 << endl;
+        ptrTemp->getData();
+        ptrTemp++;
+    }
+    
     return 0;
 }
