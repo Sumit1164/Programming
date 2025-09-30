43488cf14978828fe7dcf8974d518795b077e6b3
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-30 22:39:19 +0530

    Update comments in polymorphism.cpp for clarity and additional context

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -5,5 +5,7 @@ int main(){
     // Polymorphism means one name multiple forms
     // Ex- Water -> (cold) -> Ice  ||  Water -> (hot) -> Air 
     // Ex- Function overloading, Operator overloading, Virtual functions
+
+    // About Polymorphism it's type and ex of polymorphism, Please checkout ->  Polymorphism.png 
     return 0;
 }
