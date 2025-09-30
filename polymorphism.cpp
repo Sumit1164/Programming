cc2319c7a57bc17927974292533477cc652dfff6
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-30 21:07:21 +0530

    Add initial implementation of polymorphism example in polymorphism.cpp

---
+++ polymorphism.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // Polymorphism means one name multiple forms
+    // Ex- Water -> (cold) -> Ice  ||  Water -> (hot) -> Air 
+    // Ex- Function overloading, Operator overloading, Virtual functions
+    return 0;
+}
