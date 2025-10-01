e9badf3919b63e3d553ff67afbcf31e53376ba26
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-01 14:29:08 +0530

    This file have various basic programs such as- Print number 101 to 200, Print A to Z, Print number in reverse order,Print number from 1 to 100 but difference is 3, Multiplication table,Calculate power of a number, Sum of n natural numbers, Sum of n natural number squares, Factorial of n numbers, Prime number, Fibonacci Series of a number

--- ArmstrongNumber.cpp
+++ ArmstrongNumber.cpp
@@ -1,160 +1,160 @@
-// Print number 101 to 200
+// // Print number 101 to 200
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n, end = 200;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = n; i<= end; i++)
-    {
-        cout << i<<endl;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, end = 200;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = n; i<= end; i++)
+//     {
+//         cout << i<<endl;
+//     }
     
-}
+// }
 
 
-// Print A to Z
+// // Print A to Z
 
-#include<iostream>
-using namespace std;
-int main(){
-    char ch;
-    for ( ch = 'A'; ch <='Z'; ch++)
-    {
-        cout << ch<<endl;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     for ( ch = 'A'; ch <='Z'; ch++)
+//     {
+//         cout << ch<<endl;
+//     }
     
-}
+// }
 
 
-// Print number in reverse order
+// // Print number in reverse order
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = n; i>= 0; i--)
-    {
-        cout <<i<<endl;
-    }
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = n; i>= 0; i--)
+//     {
+//         cout <<i<<endl;
+//     }
+// }
 
-// Print number from 1 to 100 but difference is 3
+// // Print number from 1 to 100 but difference is 3
 
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 1; i <= 100; i = i+3){
-        cout << i<<endl;
-    }
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = 1; i <= 100; i = i+3){
+//         cout << i<<endl;
+//     }
+// }
 
 
-// Multiplication table 
+// // Multiplication table 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 1; i <= 10; i++){
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = 1; i <= 10; i++){
         
-        cout <<i*n << endl;
-    }
-}
+//         cout <<i*n << endl;
+//     }
+// }
 
 
-// Calculate power of a number 
-#include<iostream>
-using namespace std;
-int main(){
-    int n, pow, num;
-    cout << "Enter a number: ";
-    cin >> n;
-    cout << "Enter power: ";
-    cin >> pow;
-    num = n;
-    for (int i = 1; i < pow; i++){
-        num = num * n;
-    }
-    cout << num;
-}
+// // Calculate power of a number 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, pow, num;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     cout << "Enter power: ";
+//     cin >> pow;
+//     num = n;
+//     for (int i = 1; i < pow; i++){
+//         num = num * n;
+//     }
+//     cout << num;
+// }
 
-//  Sum of n natural numbers
+// //  Sum of n natural numbers
 
-#include <iostream>
-using namespace std;
-int main(){
-    int n, sum = 0;
-    cout<< "Enter a number: ";
-    cin >> n;
-    // 1st method
-    for (int i = 1; i <= n; i=i+1){
-        sum = sum + i;
-    }
-    cout << sum<<endl;
-    // 2nd method
-    cout << (n * (n + 1)) / 2;
-}
-
-
-//  Sum of n natural number squares
-
-#include<iostream>
-using namespace std;
-int main(){
-    int n, sum = 0;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 1; i <= n; i++){
-        sum = sum + i * i;
-    }
-    cout << sum;
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n, sum = 0;
+//     cout<< "Enter a number: ";
+//     cin >> n;
+//     // 1st method
+//     for (int i = 1; i <= n; i=i+1){
+//         sum = sum + i;
+//     }
+//     cout << sum<<endl;
+//     // 2nd method
+//     cout << (n * (n + 1)) / 2;
+// }
 
 
-// Factorial of n numbers
+// //  Sum of n natural number squares
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n, fact = 1;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 1; i <= n; i=i+1){
-        fact = fact * i;
-    }
-    cout << fact;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, sum = 0;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++){
+//         sum = sum + i * i;
+//     }
+//     cout << sum;
+// }
 
-// Prime number 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n, prime = 0;
-    cout << "Enter a number: ";
-    cin >> n;
-    if(n < 2){
-        cout << "It's not a prime number";
-        return 0;
-    }
-    for (int i = 2; i < n; i=i+1){
-        if(n % i == 0){
-            cout << "It's not a prime number";
-            return 0;
-        }
-        cout << "The given number is Prime number";
-        return 0;
-    }
-}
+// // Factorial of n numbers
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, fact = 1;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     for (int i = 1; i <= n; i=i+1){
+//         fact = fact * i;
+//     }
+//     cout << fact;
+// }
+
+// // Prime number 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, prime = 0;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     if(n < 2){
+//         cout << "It's not a prime number";
+//         return 0;
+//     }
+//     for (int i = 2; i < n; i=i+1){
+//         if(n % i == 0){
+//             cout << "It's not a prime number";
+//             return 0;
+//         }
+//         cout << "The given number is Prime number";
+//         return 0;
+//     }
+// }
 
 
 // Fibonacci Series of a number
