d413ad281f2e0429f1fa5df8f89368a46f297398
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-01 14:24:57 +0530

    Run Time Polymorphism(RTP) Explanation and example Fix variable initialization and enhance polymorphism example in polymorphism.cpp

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,3 +1,5 @@
+
+// Pointer to Derived Classes in C++
 #include<iostream>
 using namespace std;
 class Base{
@@ -25,7 +27,14 @@ int main(){
 
     Base *base_C_pointer;
     Base obj1B;
-    Derived obj2D;
     base_C_pointer = &obj2D;
+    base_C_pointer->varBase = 34;
+    base_C_pointer->display();
+
+    Derived *derived_C_pointer;
+    Derived obj2D;
+    derived_C_pointer = &obj2D;
+    derived_C_pointer->varDer = 57;
+    derived_C_pointer->display();
     return 0;
 }
