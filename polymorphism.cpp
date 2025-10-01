9e469070556d78fd161ff563f2eb2517f7d88425
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-01 15:11:44 +0530

    Compile Time Polymorphism using Function Overloading (same function name, different parameters)

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -44,7 +44,7 @@ int main(){
 
 
 
-*/
+
 
 
 
@@ -98,3 +98,45 @@ int main()
     return 0;
 }
 
+
+
+*/
+
+// Compile Time Polymorphism
+
+// Function Overloading (same function name, different parameters)
+
+#include <iostream>
+using namespace std;
+
+class Calculator
+{
+public:
+    // Function to add two integers
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Function to add three integers
+    int add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    // Function to add two floats
+    float add(float a, float b)
+    {
+        return a + b;
+    }
+};
+
+int main()
+{
+    Calculator calc;
+
+    cout << "Sum of 2 and 3: " << calc.add(2, 3) << endl;
+    cout << "Sum of 1, 2 and 3: " << calc.add(1, 2, 3) << endl;
+    cout << "Sum of 1.5 and 2.5: " << calc.add(4.5f, 2.5f) << endl;
+    return 0;
+}
