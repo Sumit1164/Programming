db4addb5346440c0ef4c1b60cc05729ad5486054
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-12 21:53:48 +0530

    File input and output example very easy code

--- FileInputOutput.cpp
+++ FileInputOutput.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 #include<fstream>
+/*
+The useful classes for working with files in C++ are:
+1. fstreambase
+2. ifstream --> derived from fstreambase
+3. ofstream --> derived from fstreambase
+*/
+
+// In order to work with files in C++, you will have to open it. Primarily, there are two ways to open a file:
+// 1. Using the constructor
+// 2. Using the member function open() of the fstream class
 using namespace std;
 int main(){
+    // string st = "This is a text to write in a file";
+    string st2;
+    // ofstream ot("FileInputOutput.txt"); // Write operation
+    // ot << st<<endl; // Write the string to the file
+    // ot.close(); // Closing the file after writing operation
+    // Opening file using constructor and reading from it
+    ifstream ik("FileInputOutput.txt"); // Read operation
+    // ik >> st2; // Write the second string to the file          --------> This show only the first word
+    getline(ik, st2); // This will get the line from the file
+    cout << st2 << endl; // Display the read string
+    getline(ik, st2); // This will get the next line from the file
+    cout << st2 << endl; // Display the read string
+    getline(ik, st2); // This will get the next line from the file
+    cout << st2 << endl; // Display the read string
     return 0;
 }
