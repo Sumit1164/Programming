a968b427c8a1f4a8fa7c6b1033a57aae9112d1b3
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-25 01:27:28 +0530

    templates in C++ use a simple programs

--- Templates.cpp
+++ Templates.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
+class vector{
+    int * arry;
+    int size;
+    public:
+    vector(int m){
+        size = m;
+        arry = new int[size];
+    }
+    int sumVec(vector & v){
+        int d = 0;
+        for (int i = 0; i < size; i++)
+        {
+            d += this->arry[i] * arry[i];
+            return d;
+        }
+        
+    }
+}
 int main(){
-    
+    vector v1(3);
+    v1.arry[0] = 5;
+    v1.arry[1  ] = 7;
+    v1.arry[2] = 57;
     return 0;
 }
