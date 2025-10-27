67350443d8c5a4a5d6e26058e9ac42510c02ac63
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-27 12:23:17 +0530

    Templates in C++ use an basics example to understand logic

--- Templates.cpp
+++ Templates.cpp
@@ -1,27 +1,35 @@
 #include<iostream>
 using namespace std;
+
+template<class T>
+
 class vector{
-    int * arry;
-    int size;
     public:
+    T * arry;
+    int size;
     vector(int m){
         size = m;
-        arry = new int[size];
+        arry = new T[size];
     }
-    int sumVec(vector & v){
-        int d = 0;
+    T sumVec(vector & v){
+        T d = 0;
         for (int i = 0; i < size; i++)
         {
-            d += this->arry[i] * arry[i];
-            return d;
+            d += this->arry[i] + v.arry[i];
         }
-        
+        return d;
     }
-}
+};
 int main(){
-    vector v1(3);
-    v1.arry[0] = 5;
-    v1.arry[1  ] = 7;
-    v1.arry[2] = 57;
+    vector <float>v1(3);
+    v1.arry[0] = 5.4;
+    v1.arry[1] = 7.5;
+    v1.arry[2] = 1.78;
+    vector <float> v2(3);
+    v2.arry[0] = 2.5;
+    v2.arry[1] = 3.7;
+    v2.arry[2] = 6.57;
+    float a = v1.sumVec(v2);
+    cout << a<< endl;
     return 0;
 }
