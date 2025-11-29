ed7353fed3622a468b0961519c1808188538250a
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-29 20:24:22 +0530

    that's all about the list, today complete list and very important methods

--- List.cpp
+++ List.cpp
@@ -20,7 +20,7 @@ int main(){
     list1.push_back(4);
 
     //! Way 1st to print the list
-    /*
+
     list<int>::iterator iter;
     iter = list1.begin();
     cout << *iter << " ";
@@ -28,52 +28,56 @@ int main(){
     cout << *iter << " ";
     iter++;
     cout << *iter << " ";
-    */
+
 
     //! Way 2nd to print the list
-    /*
+
     for (int x : list1)
     {
         cout << x << endl;
     }
-    */
+
 
     //! Way 3rd to print the list
-    // display(list1);
+    display(list1);
 
     //? Delete an element from the list at the end
-    // list1.pop_back();
+    list1.pop_back();
     //? Delete an element from the list at the front
-    // list1.pop_front();
+    list1.pop_front();
     //? Delete an element from middle in the list
-    // list1.remove(4);
+    list1.remove(4);
 
 
     //* Sorting the list
-    // list1.sort();
+    list1.sort();
     display(list1);
 
     //* Create another list 
     list<int> list2(3); // Empty list of size 3
     list<int>::iterator itr;
     itr = list2.begin();
-    *itr = 4.5;
+    *itr = 4;
     itr++;
-    *itr = 5.7;
+    *itr = 5;
     itr++;
-    *itr = 7.5;
+    *itr = 7;
     itr++;
     display(list2);
 
 
     //? Merging two or more lists
-    // list1.merge(list2);
-    // cout << "After merging the list: "<<endl ;
+    list1.merge(list2);
+    cout << "After merging the list: "<<endl ;
 
 
     //? Reversing the list
-    // list1.reverse();
+    list1.reverse();
     display(list1);
+
+    //? Checking the list is empty or not if list don't have an element the return 1, if list having an element then return 0
+    list2.push_back(5);
+    cout << list2.empty();
     return 0;
 }
 
