14991cec147d0ded275c8b3d128c1e54f10c38fa
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-29 21:59:19 +0530

    complete map with simple example as well use map methods

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,30 @@
+//! Map is an associative array
+
 #include<iostream>
+#include<string>
+#include<map>
+
 using namespace std;
 int main(){
+    map<string, int> marksMap;
+    marksMap["Sumit"] = 98;
+    marksMap["Kashish"] = 99;
+    marksMap["Shubh"] = 90;
+    marksMap["Kajal"] = 95;
+
+    //* Insert new person in the map
+    marksMap.insert({{"Armaan", 99}, {"Anshika", 95}});
+
+    map<string, int>::iterator itra;
+    for (itra = marksMap.begin(); itra != marksMap.end(); itra++){
+        cout << (*itra).first << " " << (*itra).second << "\n";
+    }
+    cout << "The size of map is: " << marksMap.size()<<endl;
+    cout << "The Max-size of map is: " << marksMap.max_size()<<endl;
+    cout << "The empty value of map: " << marksMap.empty()<<endl;
+
+    auto result = marksMap.emplace("i", 1); // key: "i", value: 1
+    cout << "Inserted? " << boolalpha << result.second << "\n";
+    cout << "Key: " << result.first->first << ", Value: "<< result.first->second << "\n";
     return 0;
 }
