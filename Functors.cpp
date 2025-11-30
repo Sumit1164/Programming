fee5ea4285ed84096c4becbcd82181289b603ce8
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-30 21:14:06 +0530

    Function object (Functors) in C++ STL

--- Functors.cpp
+++ Functors.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main(){
     int arrays[] = {70, 40, 90, 45, 23, 1, 2};
     int n = sizeof(arrays) / sizeof(arrays[0]);
-    sort(arrays, arrays + 5); // assending order
-    sort(arrays, arrays + 7, greater<int>()); // dessending order
+    // sort(arrays, arrays + 5); // assending order
+    // sort(arrays, arrays + 7, greater<int>()); // dessending order
     for (int i = 0; i < n; i++)
     {
         cout << "The array is: " << arrays[i] << endl;
@@ -18,5 +18,7 @@ int main(){
     return 0;
 }
 
+// C++reference 
+// algorithm stl
 
 //? Function Object : Function wrapped in a class so that it available like an object
